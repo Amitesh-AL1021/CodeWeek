cfd2593202cc4bb3164017aa519c36877899571b
Amitesh Thakur <tamitesh35@gmail.com>
2026-06-13 16:29:48 +0530

    factorial

---
+++ Day_01/Factorial.py
@@ -0,0 +1,14 @@
+# def factorial(num):
+#     if num == 0:
+#         return 1 
+#     else:
+#         return num * factorial(num-1)
+# num = 3
+# print(factorial(num))
+
+
+num = 5
+result = 1 
+for i in range(1,num+1):
+    result = result *i
+print(result)
